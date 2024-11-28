52be6903888dc12da8d3a8248314234df47134fe
Daniel D'Costa <dcostd1@mcmaster.ca>
2024-11-28 16:59:37 -0500

    message

--- Project.cpp
+++ Project.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "Food.h"
+#include "GameMechs.h"
+#include "objPos.h"
+#include "objPosArrayList.h"
+#include "Player.h"
+
 
 using namespace std;
 
@@ -15,6 +21,10 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
+GameMechs *board;
+Player *p;
+Food *f;
+
 
 
 int main(void)
@@ -22,7 +32,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(board->getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -40,22 +50,67 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    board = new GameMechs();
+    p = new Player(board);
+    f = new Food(board);
+    p->drawPlayer();
+    f->generateFood(p->getPlayerPos())
+
+    // exitFlag = false;
 }
 
 void GetInput(void)
 {
-   
+    if(MacUILib_hasChar())
+    {
+        board->setInput(MacUILib_getChar());
+    }
+    else
+    {
+        board->setInput(0);
+    }
 }
 
 void RunLogic(void)
 {
-    
+    switch(board->getInput())
+    {
+        case 0:
+            break;
+        case ' ':
+            board->setExitTrue();
+            break;
+        case 'q': //debug key for incrementing score
+            board->incrementScore();
+            break;
+        case 'f':
+            f->generateFood(p->getPlayerPos());
+            break;
+        default:
+            p->updatePlayerDir();
+            break;
+    }
+    p->movePlayer();
+    p->drawPlayer();
+    board->clearInput();
 }
 
 void DrawScreen(void)
 {
     MacUILib_clearScreen();    
+
+    int xBoundary = board->getBoardSizeX();
+    int yBoundary = board->getBoardsizeY();
+    int i, j;
+
+    for(i = 0; i < yBoundary; i++)
+    {
+        for(j = 0; j < xBoundary; j++)
+        {
+            MacUILib_printf("%c", board->getBoard(i,j));
+        }
+        MacUILib_printf
+    }
 }
 
 void LoopDelay(void)
